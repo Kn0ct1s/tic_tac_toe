fdaf21a6b18cbdf32eb7524cd1525d867f95a6d6
N0ct1s <n0ct1s-prgrmmr@pm.me>
2023-06-17 20:17:59 +0100

    can place x and o's

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <limits>
 
 class TicBoard {
     private:
         char m_board[3][3]{};
 
     public:
+        bool in_game{false};
+
         // constructor
         TicBoard() = default;
 
         // forward declarations
         char& operator()(int row, int col);
+        void operator()(int row, int col, char player);
 
         // friend functions
         friend std::ostream& operator<<(std::ostream& out, const TicBoard& board);
@@ -28,33 +32,65 @@ char& TicBoard::operator()(int row, int col) {
     return m_board[row][col];
 }
 
+void TicBoard::operator()(int row, int col, char player) {
+    m_board[row][col] = player;
+}
+
+// REFACTOR
 std::ostream& operator<<(std::ostream& out, const TicBoard& board) {
     for (auto& row : board.m_board) {
         for (auto& column : row) {
-            out << column;
+            out << column << '\t';
         }
         out << '\n';
     }
     return out;
 }
 
-struct Player {
-    // either an 'X' or an 'O'
-    char side{};
-    // unsigned as the player will never be in negetive points
-    unsigned int points{};
-};
+void get_pos(int& x, int &y) {
+    std::cin >> x;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), ',');
+    std::cin >> y;
 
-
+    if (x) {
+        --x;
+    } 
+    if (y) {
+        --y;
+    }
+}
 
 
 int main() {
     TicBoard board{};
 
-    Player x{'X', 0};
-    Player o{'O', 0};
+    int x_score{0};
+    int y_score{0};
 
     board.init_board();
 
-    std::cout << board(1, 1);
+    board.in_game = true;
+
+    std::cout << "Tic Tac Toe\n";
+
+    int x{};
+    int y{};
+
+    while (board.in_game) {
+        std::cout << "\n";
+
+        std::cout << "X choose a tile to place (x, y): ";        
+        get_pos(x, y);
+        board(x, y, 'X');
+
+        std::cout << board;
+
+        std::cout << "O choose a tile to place (x, y): ";
+        get_pos(x, y);
+        board(x, y, 'O');
+
+        std::cout << board;
+
+        break;
+    }
 }
