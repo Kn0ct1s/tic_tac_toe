57a9ea26d8040e84710046ba6ac9909a191a75fe
N0ct1s <n0ct1s-prgrmmr@pm.me>
2023-06-19 02:32:58 +0100

    basic game loop is finished.
    Need to add some error checking, featurese and optomize the code.
    I also need to make it more modular and not all in one file.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ class TicBoard {
         char m_board[3][3]{};
 
     public:
-        bool in_game{false};
 
         // constructor
         TicBoard() = default;
@@ -109,39 +108,49 @@ std::ostream& operator<<(std::ostream& out, const TicBoard& board) {
 int main() {
     TicBoard board{};
     
-    Player x{'x', 0};
-    Player o{'o', 0};
+    Player x{'X', 0};
+    Player o{'O', 0};
 
     board.init_board();
 
-    board.in_game = true;
-
     while (true) {
-        std::cout << "Tic Tac Toe\n";
+        std::cout << "\tTic Tac Toe\n\n";
+        std::cout << "X points: " << x.points << "\t" << "O points: " << o.points;
 
-        while (board.in_game) {
-
+        while (true) {
             std::cout << board;
-            board.place_piece(x);
 
+            board.place_piece(x);
+            std::cout << board;
             if (board.game_won(x)) {
-                std::cout << "x won";
-                std::cout << board;
+                std::cout << "X won!\n\n";
                 break;
             }
-            
 
-            std::cout << board;
             board.place_piece(o);
-
             std::cout << board;
-
             if (board.game_won(o)) {
-                std::cout << "o won";
-                std::cout << board;
+                std::cout << "O won!\n\n";
                 break;
             }
         }
-    break;
+
+        while (true) {
+            std::cout << "Play again? (y/n): ";
+            char ans{};
+            std::cin >> ans;
+
+            switch (ans) {
+                case 'y': {
+                    board.init_board();
+                    break;
+                }
+                case 'n':
+                    std::cout << "X Scored: " << x.points << '\n';
+                    std::cout << "O Scored: " << o.points << '\n';
+                    exit(0);
+            }
+            break;
+        }
     }
 }
