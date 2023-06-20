b0da32cbf0f0e198ca5eb9abcda16421f2f09b7d
N0ct1s <n0ct1s-prgrmmr@pm.me>
2023-06-20 10:28:36 +0100

    added more input handling

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 #include "TicBoard.h"
 
@@ -44,9 +45,14 @@ int main() {
                     break;
                 }
                 case 'n':
-                    std::cout << "X Scored: " << x.points << '\n';
+                    std::cout << "\nX Scored: " << x.points << '\n';
                     std::cout << "O Scored: " << o.points << '\n';
                     exit(0);
+                default:
+                    std::cout << "Invalid input, try again\n\n";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    continue;
             }
             break;
         }
